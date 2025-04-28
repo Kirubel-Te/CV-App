016a012eb692f5c35f1a8c0416f6ab3e6970eb53
Kirubel Tesfaye <kirubeltesfaye519@gmail.com>
2025-04-28 20:43:30 +0300

    changehandler

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -29,6 +29,15 @@ export default function Form(){
         setcv(prev => !prev)
     }
 
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setform(prevForm => ({
+            ...prevForm,
+            [name]: value
+        }));
+    }
+    
+
     return(
         <>
             {cv ? 
@@ -40,12 +49,12 @@ export default function Form(){
                         <hr/>
                     </div>
                     <div className="name">
-                        <Label htmlFor='f-name' name='fname' desc='First Name: ' type='text' value={form.fname} />
-                        <Label htmlFor='l-name' name='lname' desc='Last Name: ' type='text' value={form.lname} />
+                        <Label htmlFor='f-name' name='fname' desc='First Name: ' type='text' value={form.fname} onChange={handleChange} />
+                        <Label htmlFor='l-name' name='lname' desc='Last Name: ' type='text' value={form.lname} onChange={handleChange} />
                     </div>
                     <div className="tele">
-                        <Label htmlFor='email' name='email' desc='E-mail: ' type='email' value={form.email} />
-                        <Label htmlFor='phonenumber' name='phonenumber' desc='Phone Number: ' type='tel' value={form.phonenumber} />
+                        <Label htmlFor='email' name='email' desc='E-mail: ' type='email' value={form.email} onChange={handleChange} />
+                        <Label htmlFor='phonenumber' name='phonenumber' desc='Phone Number: ' type='tel' value={form.phonenumber} onChange={handleChange} />
                     </div>
                 </section>
                 <section className="education">
@@ -54,13 +63,13 @@ export default function Form(){
                         <hr/>
                     </div>
                     <div className="school">
-                        <Label htmlFor="sName" name='sName' desc='School Name: ' type='text' value={form.sName} />
+                        <Label htmlFor="sName" name='sName' desc='School Name: ' type='text' value={form.sName} onChange={handleChange}/>
                     </div>
                     <div className="study">
-                        <Label htmlFor='study' name='study' desc='Title of Study: ' type='text' value={form.study} />
+                        <Label htmlFor='study' name='study' desc='Title of Study: ' type='text' value={form.study} onChange={handleChange}/>
                     </div>
                     <div className="date">
-                        <Label htmlFor='date' name='date' desc='Expected Graduation: ' type='date' value={form.date} />
+                        <Label htmlFor='date' name='date' desc='Expected Graduation: ' type='date' value={form.date} onChange={handleChange}/>
                     </div>
                 </section>
                 <section className="practical">
@@ -69,13 +78,13 @@ export default function Form(){
                         <hr/>
                     </div>
                     <div className="CName">
-                        <Label htmlFor='cname' name='cname' desc='Company Name: ' type='text' value={form.cname} />
+                        <Label htmlFor='cname' name='cname' desc='Company Name: ' type='text' value={form.cname} onChange={handleChange}/>
                     </div>
                     <div className="position">
-                        <Label htmlFor='position' name='position' desc='Position: ' type='text' value={form.position} />
+                        <Label htmlFor='position' name='position' desc='Position: ' type='text' value={form.position} onChange={handleChange}/>
                     </div>
                     <div className="role">
-                        <Label htmlFor='role' name ='role' desc='Responsiblity: ' type='text' value={form.role} />
+                        <Label htmlFor='role' name ='role' desc='Responsiblity: ' type='text' value={form.role} onChange={handleChange}/>
                     </div>
                 </section>
                 <section className="submit">
--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -2,7 +2,7 @@ export default function Label(props){
     return(
         <>
             <label htmlFor={props.htmlFor}>{props.desc} </label><br/>
-            <input id={props.htmlFor} type={props.type} name={props.name} value={props.value} required/>
+            <input id={props.htmlFor} type={props.type} name={props.name} value={props.value} onChange={props.onChange} required/>
         </>
     )
 }
